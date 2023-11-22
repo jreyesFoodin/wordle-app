e252e80924d5e98a91b809426bb2cb0dfaa9bb61
Juan Reyes Celestino <juanreyescelestino@Juans-MacBook-Air.local>
2023-11-22 14:57:26 -0500

    [add] funcion para buscar la palabra del dia

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react'
+import wordArray from './constants/constants.json'
 const App = () => {
+  const [words, setWords] = useState([])
+  useEffect(() => {
+    getWordsFiveLetters()
+  }, [])
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
+  const getWordsFiveLetters = () => {
+    const shuffledArray = shuffleArray([...wordArray])
+    const palabrasCincoLetras = shuffledArray.filter((palabra) => palabra.length === 5).slice(0, 1)
+    setWords(palabrasCincoLetras[0])
+  }
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello world!
-      </h1>
+
     </>
   )
 }
