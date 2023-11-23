cd3b992d3be02633d3c3d37bcd80f1e2d0476bf1
Juan Reyes Celestino <juanreyescelestino@Juans-MacBook-Air.local>
2023-11-23 13:54:53 -0500

    [add] se creo componente de alertas

---
+++ src/components/Alert.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { hideAlert } from '../redux/conf/alertSlice'
+
+const Alert = () => {
+  const dispatch = useDispatch()
+  const { showAlert, message, alertType } = useSelector((state) => state)
+
+  useEffect(() => {
+    let timeout
+
+    if (showAlert) {
+      timeout = setTimeout(() => {
+        dispatch(hideAlert())
+      }, 5000)
+    }
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [dispatch, showAlert])
+
+  return (
+    showAlert && (
+      <div className={`alert alert-${alertType}`}>
+        <p>{message}</p>
+      </div>
+    )
+  )
+}
+
+export default Alert
