9808d3ac0c6fa79399b7d2d83ef2b29adce3bcf8
Juan Reyes Celestino <juanreyescelestino@Juans-MacBook-Air.local>
2023-11-23 13:52:42 -0500

    [add] se creo y vinculo el redux de alert

---
+++ src/redux/conf/alertSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+const initialState = {
+  showAlert: false,
+  message: '',
+  alertType: 'info'
+}
+
+const userAlert = createSlice({
+  name: 'alert',
+  initialState,
+  reducers: {
+    showAlert: (state, action) => {
+      return {
+        showAlert: true,
+        message: action.payload.message,
+        alertType: action.payload.alertType || 'info'
+      }
+    },
+    hideAlert: (state) => {
+      return initialState
+    }
+  }
+})
+
+export const { showAlert, hideAlert } = userAlert.actions
+export default userAlert.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,23 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
 import confReducer from './conf/confSlice'
 import historyReducer from './conf/historySlice'
 import countDownReducer from './conf/countDownSlice'
-import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
-import thunk from 'redux-thunk'
+import alertReducer from './conf/alertSlice'
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['confState', 'historyState', 'countDownState']
+  whitelist: ['confState', 'historyState', 'countDownState', 'alertState']
 }
 
 const rootReducer = combineReducers({
   confState: confReducer,
   historyState: historyReducer,
-  countDownState: countDownReducer
+  countDownState: countDownReducer,
+  alertState: alertReducer
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
